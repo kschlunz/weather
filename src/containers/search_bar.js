a90b0ae29f6e4bd138f4c88cb8fe2104ff647311
github username <github email address>
2018-08-16 10:01:19 -0400

    Added Search Bar Component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component{
+
+  render(){
+    return(
+      <form className="input-group">
+        <input />
+      </form>
+    )
+  }
+
+}
