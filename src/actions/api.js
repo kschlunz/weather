35d78746e99b9f8d31de13dd80681d5c8d4829b3
github username <github email address>
2018-08-16 11:37:48 -0400

    Added API ROOT URL

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,1 +1,10 @@
 const API_KEY = '4714f6f17f5c1c7e43c61476c511d581'
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(){
+  return {
+    type: FETCH_WEATHER
+  };
+}
