082d1cb1e9445f40290bb0867994ff9a99f008ba
github username <schlunzk@gmail.com>
2018-08-17 09:58:26 -0400

    added table with data on weather_list container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux'
 
-export default class WeatherList extends Component{
+class WeatherList extends Component{
+
+
+  renderWeather = (cityData) => {
+    const name = cityData.city.name
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    )
+  }
+
   render(){
     return(
       <table className= "table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 
 }
+
+function mapStateToProps(state){
+
+  return { weather: state.weather }
+}
+
+export default connect(mapStateToProps)(WeatherList)
