603a62d811ca7422fc116504f4bc3bb5e4d309ee
github username <schlunzk@gmail.com>
2018-08-17 14:49:36 -0400

    added average line to chart. average numbers. wrote function to convert Kelvin to Fahrenheit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -9,15 +9,27 @@ class WeatherList extends Component{
 
   renderWeather = (cityData) => {
     const name = cityData.city.name
-    const temps = cityData.list.map(weather => weather.main.temp)
-    console.log(temps)
+    const temps = cityData.list.map(weather => weather.main.temp) // go back and write a function to change this to fahrenheit (currently using Kelvin)
+    const pressures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
 
+
+    function fahrenheit(temps){
+      let f = 0
+      let ftemps = []
+          temps.map(i =>{
+          f = (i * 9/5) - 459.67
+          ftemps.push(Math.round(f))
+          })
+      return ftemps
+    }
+    
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="blue"/>
-        </td>
+        <td><Chart data={fahrenheit(temps)} color="green"/></td>
+        <td><Chart data={pressures} color="orange"/></td>
+        <td><Chart data={humidities} color="red"/></td>
       </tr>
     )
   }
@@ -28,9 +40,9 @@ class WeatherList extends Component{
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hP)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
