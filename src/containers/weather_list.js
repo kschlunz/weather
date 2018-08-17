ae0296334ecdfd7d821e35d22c23c8f8ca965fbe
github username <schlunzk@gmail.com>
2018-08-17 10:36:37 -0400

    added Sparklines library for charts and added to table with data being use

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component{
 
 
   renderWeather = (cityData) => {
     const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
+    console.log(temps)
+
     return(
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="blue"/>
+          </Sparklines>
+        </td>
       </tr>
     )
   }
