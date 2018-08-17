713c0798b6c5184aac98f58034117a22f07ee2b7
github username <schlunzk@gmail.com>
2018-08-17 09:29:26 -0400

    started weather_list container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './containers/search_bar'
+import WeatherList from './containers/weather_list'
 
 class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
