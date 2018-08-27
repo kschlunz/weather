c306e79c0df0ea41929bf029a639b74c609eb7a1
github username <schlunzk@gmail.com>
2018-08-27 14:53:06 -0400

    added a card for the breweries information, need to now organize the page with a grid system, search bar, maps etc

--- src/components/beer.js
+++ src/components/beer.js
@@ -5,7 +5,17 @@ const Beer = (props) => {
   const beerName = props.data.map( b => b.name)
   const brewery = props.data.map(b => {
       if(b.status === "Brewery"){
-        return b.name
+        return (
+          <div className="card" style={{width: 30 + 'rem' }}>
+          <div className="card-body">
+    <h1 className="card-title">{b.name}</h1>
+    <h6 className="card-subtitle mb-2 text-muted">{b.street}</h6>
+    <h6 className="card-subtitle mb-2 text-muted">{b.city} {b.state} {b.zip} </h6>
+    <h6 className="card-subtitle mb-2 text-muted">{b.phone} </h6>
+    <a href={'https://www.'+b.url} target="_blank" rel="noopener noreferrer" className="card-link">Brewery Info</a>
+  </div>
+</div>
+        )
       }
   })
 
--- src/containers/hiking_trails.js
+++ src/containers/hiking_trails.js
@@ -13,22 +13,32 @@ class HikingTrails extends Component {
     if(this.props.trails.trails){
       const trail= this.props.trails.trails.map(t => {
         return(
-          <div className="card" style={{width: 18 + 'rem' }}>
-          <img className="card-img-top" src={t.imgMedium ? ( t.imgMedium ) : ("http://appalachiantrail.org/images/default-source/default-album/trailfocus.jpg?sfvrsn=2")} />
-          <div className="card-body">
-    <h1 className="card-title">{t.name}</h1>
-    <p className="card-text">{t.conditionDetails}</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">Cras justo odio</li>
-    <li className="list-group-item">Dapibus ac facilisis in</li>
-    <li className="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div className="card-body">
-    <a href="#" className="card-link">Card link</a>
-    <a href="#" className="card-link">Another link</a>
-  </div>
+
+          <div className = "card-group">  
+            <div className="card" style={{width: 50 + 'rem' }}>
+              <img className="card-img-top" src={t.imgMedium ? ( t.imgMedium ) : ("http://appalachiantrail.org/images/default-source/default-album/trailfocus.jpg?sfvrsn=2")} />
+                <div className="card-body">
+                  <h1 className="card-title">{t.name}</h1>
+                    <h2 className="card-text">{t.location} </h2>
+                      <h3 className="card-text">Difficulty: {t.difficulty} </h3>
+                        <h5 className="card-text">{t.summary} </h5>
+                        </div>
+              <ul className="list-group list-group-flush">
+              <li className="list-group-item">Length: {t.length} miles</li>
+              <li className="list-group-item">Ascent: {t.ascent} ft, Descent: {t.descent} ft</li>
+              <li className="list-group-item">Conditions: {t.conditionStatus}, {t.conditionDetails} </li>
+              <li className="list-group-item">High: {t.high} ft, Low: {t.low}</li>
+              <li className="list-group-item">Stars: {t.stars}</li>
+              </ul>
+
+        <div className="card-body">
+        <a href={t.url} target="_blank" rel="noopener noreferrer" className="card-link">Trail Information</a>
+        </div>
+
+    </div>
+
 </div>
+
 
         )
       })
