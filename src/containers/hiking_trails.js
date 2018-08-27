42208051608dfb621549236961eaa978bcc53d85
github username <schlunzk@gmail.com>
2018-08-27 13:55:49 -0400

    refactored mapping over hiking trail data to include creating a new card with the information on the hiking_trails component

--- src/containers/hiking_trails.js
+++ src/containers/hiking_trails.js
@@ -2,30 +2,45 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { bindActionCreators, compose, createStore, applyMiddleware } from 'redux';
 import { hikingTrails } from '../actions/api'
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
 
 class HikingTrails extends Component {
 
   renderTrails = () => {
     console.log("TRAILS", this.props.trails.trails)
     if(this.props.trails.trails){
-      const trail= this.props.trails.trails.map(t => t.name)
-      const trailName = trail.map(name => name)
-      const trailPic = this.props.trails.trails.map(t => t.imgMedium)
-      const pictures = trailPic.map(pic => pic)
+      const trail= this.props.trails.trails.map(t => {
+        return(
+          <div className="card" style={{width: 18 + 'rem' }}>
+          <img className="card-img-top" src={t.imgMedium ? ( t.imgMedium ) : ("http://appalachiantrail.org/images/default-source/default-album/trailfocus.jpg?sfvrsn=2")} />
+          <div className="card-body">
+    <h1 className="card-title">{t.name}</h1>
+    <p className="card-text">{t.conditionDetails}</p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+</div>
 
-      console.log(pictures)
-      console.log(trailName)
+        )
+      })
+
+
+
 
 
       return(
-          // <img className="card-img-top" src= {`${pictures}`}></img>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{trailName}</h5>
-              <p className="card-text">Some info</p>
-          </div>
+        <div>
+          {trail}
         </div>
-
       )
 
     }
